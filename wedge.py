14121471c4cfa965dd36da4b930fe69f5e6e0cb6
Kelcey <kelcey@kelcey.net>
2015-01-23 11:04:00 -0700

    version1

---
+++ wedge.py
@@ -0,0 +1,46 @@
+import time
+import serial
+
+# configure the serial connections (the parameters differs on the device you are connecting to)
+ser = serial.Serial(
+    port='/dev/ttyS4',
+    baudrate=9600,
+    parity=serial.PARITY_EVEN,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.SEVENBITS
+)
+
+#ser.open()
+ser.isOpen()
+#print("connected to: " + ser.portstr)
+
+ser.write('W\r\n')
+
+
+time.sleep(1)
+
+out = ''
+while ser.inWaiting() > 0:
+	out += ser.read(1)
+
+out = out.replace(str(unichr(2)), "")
+
+
+
+print out
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ser.close()
+
